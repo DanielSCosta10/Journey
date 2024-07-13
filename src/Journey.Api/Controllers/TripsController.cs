9a2d352a88b2602dc6b1ec3dfd146ae5241d994f
Daniel Costa <danielscosta05@gmail.com>
2024-07-13 14:25:56 -0300

    Initial commit

---
+++ src/Journey.Api/Controllers/TripsController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Journey.Communication.Requests;
+using Journey.Application.UseCases.Trips.Register;
+using Journey.Exception.ExceptionsBase;
+using Journey.Application.UseCases.Trips.GetAll;
+using Journey.Application.UseCases.Trips.GetById;
+using Journey.Communication.Responses;
+using Journey.Application.UseCases.Trips.Delete;
+
+namespace Journey.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseShortTripJson), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status400BadRequest)]
+        public IActionResult Register([FromBody] RequestRegisterTripJson request)
+        {
+           
+            var useCase = new RegisterTripUseCase();
+
+            var response = useCase.Execute(request);
+
+            return Created(string.Empty, response);
+
+
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(ResponseTripsJson), StatusCodes.Status200OK)]
+        public IActionResult GetAll()
+        {
+            var useCase = new GetAllTripsUseCase();
+
+            var response = useCase.Execute();
+
+            return Ok(response);
+        }
+
+        [HttpGet()]
+        [Route("{id}")]
+        [ProducesResponseType(typeof(ResponseTripJson), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status404NotFound)]
+
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            
+            var useCase = new GetTripByIdUseCase();
+
+            var response = useCase.Execute(id);
+
+            return Ok(response);
+            
+        }
+
+        [HttpDelete()]
+        [Route("{id}")]
+        [ProducesResponseType( StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status404NotFound)]
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+
+            var useCase = new DeleteTripByIdUseCase();
+
+            useCase.Execute(id);
+
+            return NoContent();
+
+        }
+
+        [HttpPost]
+        [Route("{tripId}/activity")]
+        [ProducesResponseType(typeof(ResponseActivityJson), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status404NotFound)]
+        public IActionResult RegisterActivity([FromRoute] Guid tripId, [FromBody] RequestRegisterActivityJson request)
+        {
+
+            return Created();
+        }
+    }
+}
---
+++ src/Journey.Application/UseCases/Activities/Register/RegisterActivityForTripUseCase.cs
@@ -0,0 +1,52 @@
+﻿using Journey.Communication.Requests;
+using Journey.Communication.Responses;
+using Journey.Exception.ExceptionsBase;
+using Journey.Exception;
+using Journey.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Journey.Infrastructure.Entities;
+using FluentValidation.Results;
+
+namespace Journey.Application.UseCases.Activities.Register;
+
+internal class RegisterActivityForTripUseCase
+{
+    public ResponseActivityJson Execute(Guid tripId, RequestRegisterActivityJson resquest)
+    {
+        var dbContext = new JourneyDbContext();
+
+        var trip = dbContext
+            .Trips
+            .Include(trip => trip.Activities)
+            .FirstOrDefault(trip => trip.Id == tripId);
+
+        if (trip is null)
+        {
+            throw new NotFoundException(ResourceErrorMessages.TRIP_NOT_FOUND);
+        }
+
+
+
+        return null;
+    }
+
+    private void Validate(Trip trip, RequestRegisterActivityJson resquest)
+    {
+        var validator = new RegisterActivityValidator();
+
+        var result = validator.Validate(resquest);
+
+        if ((resquest.Date >= trip.StartDate && resquest.Date <= trip.EndDate) == false)
+        {
+            result.Errors.Add(new ValidationFailure("Date", Res));
+        }
+
+        if (result.IsValid == false)
+        {
+            var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+
+            throw new ErrorOnValidationException(errorMessages);
+        }
+    }
+
+}
